86ba23d39693873a1bf4059469628ac6514b7d23
vivekgoyal-1 <154058616+vivekgoyal-1@users.noreply.github.com>
2025-07-17 13:33:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
             <Toaster richColors />
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                <p>Made with 💗 by Vivek</p>
               </div>
             </footer>
           </ThemeProvider>
